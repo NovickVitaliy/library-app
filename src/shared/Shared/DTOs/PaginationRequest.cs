a61268c36a0070125d5c29c1a313ab9f0a8511df
Vitalii Novik <novickvitaliy@gmail.com>
2025-10-11 21:06:53 +0300

    update pagination request

--- src/shared/Shared/DTOs/PaginationRequest.cs
+++ src/shared/Shared/DTOs/PaginationRequest.cs
@@ -3,5 +3,5 @@ using System.ComponentModel.DataAnnotations;
 namespace Shared.DTOs;
 
 public record PaginationRequest(
-    [Range(1, int.MaxValue, ErrorMessage = "Page size must be a positive number")] int PageSize = 10,
+    [Range(1, 50, ErrorMessage = "Page size must be a positive number")] int PageSize = 10,
     [Range(1, int.MaxValue, ErrorMessage = "Page number must be a positive number")] int PageNumber = 1);
