664b6173b883e087837ee587c6b01b857b5f7568
Vitalii Novik <novickvitaliy@gmail.com>
2025-10-23 17:21:59 +0300

    implement commands for books

--- src/services/BookCatalog/BookCatalog.Application/Validators/Books/CreateBookCommandValidator.cs
+++ src/services/BookCatalog/BookCatalog.Application/Validators/Books/CreateBookCommandValidator.cs
@@ -1,17 +1,13 @@
 using BookCatalog.Application.DTOs.Books.Requests;
-using BookCatalog.Application.UseCases.Commands.Books.Update;
+using BookCatalog.Application.UseCases.Commands.Books.Create;
 using FluentValidation;
 
 namespace BookCatalog.Application.Validators.Books;
 
-public class UpdateBookRequestValidator : AbstractValidator<UpdateBookCommand>
+public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
 {
-    public UpdateBookRequestValidator()
+    public CreateBookCommandValidator()
     {
-        RuleFor(x => x.BookId)
-            .NotEmpty()
-            .WithMessage("Book ID is required");
-        
         RuleFor(x => x.Request.Title)
             .NotEmpty().WithMessage("Title is required.")
             .MaximumLength(200);
@@ -19,35 +15,35 @@ public class UpdateBookRequestValidator : AbstractValidator<UpdateBookCommand>
         RuleFor(x => x.Request.Author)
             .NotEmpty().WithMessage("Author is required.")
             .MaximumLength(200);
-
+        
         RuleFor(x => x.Request.Pages)
             .GreaterThan(0).WithMessage("Pages must be greater than 0.");
-
+        
         RuleFor(x => x.Request.Price)
             .GreaterThan(0).WithMessage("Price must be greater than 0.");
-
+        
         RuleFor(x => x.Request.ShippingCost)
             .GreaterThanOrEqualTo(0).WithMessage("Shipping cost cannot be negative.");
-
+        
         RuleFor(x => x)
             .Must(BeEitherPhysicalOrDigital)
             .WithMessage("A book must be either physical or digital, not both.");
-
+        
         When(IsPhysicalBook, () =>
         {
             RuleFor(x => x.Request.Weight)
                 .NotNull().WithMessage("Weight is required for physical books.")
                 .GreaterThan(0).WithMessage("Weight must be greater than 0.");
-
+        
             RuleFor(x => x.Request.ShelfLocation)
                 .NotEmpty().WithMessage("Shelf location is required for physical books.");
         });
-
+        
         When(IsDigitalBook, () =>
         {
             RuleFor(x => x.Request.FileFormat)
                 .NotEmpty().WithMessage("File format is required for digital books.");
-
+        
             RuleFor(x => x.Request.DownloadLink)
                 .NotEmpty().WithMessage("Download link is required for digital books.")
                 .Must(link => Uri.IsWellFormedUriString(link, UriKind.Absolute))
@@ -55,7 +51,7 @@ public class UpdateBookRequestValidator : AbstractValidator<UpdateBookCommand>
         });
     }
 
-    private static bool BeEitherPhysicalOrDigital(UpdateBookCommand request)
+    private static bool BeEitherPhysicalOrDigital(CreateBookCommand request)
     {
         var isPhysical = request.Request.Weight.HasValue || !string.IsNullOrWhiteSpace(request.Request.ShelfLocation);
         var isDigital = !string.IsNullOrWhiteSpace(request.Request.FileFormat) || !string.IsNullOrWhiteSpace(request.Request.DownloadLink);
@@ -63,14 +59,14 @@ public class UpdateBookRequestValidator : AbstractValidator<UpdateBookCommand>
         return isPhysical ^ isDigital;
     }
 
-    private static bool IsPhysicalBook(UpdateBookCommand request)
+    private static bool IsPhysicalBook(CreateBookCommand request)
     {
         var isPhysical = request.Request.Weight.HasValue || !string.IsNullOrWhiteSpace(request.Request.ShelfLocation);
         var isDigital = !string.IsNullOrWhiteSpace(request.Request.FileFormat) || !string.IsNullOrWhiteSpace(request.Request.DownloadLink);
         return isPhysical && !isDigital;
     }
 
-    private static bool IsDigitalBook(UpdateBookCommand request)
+    private static bool IsDigitalBook(CreateBookCommand request)
     {
         var isPhysical = request.Request.Weight.HasValue || !string.IsNullOrWhiteSpace(request.Request.ShelfLocation);
         var isDigital = !string.IsNullOrWhiteSpace(request.Request.FileFormat) || !string.IsNullOrWhiteSpace(request.Request.DownloadLink);
