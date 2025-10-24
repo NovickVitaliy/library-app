6246019069b94463acd4499086f78bf166cf0f15
Vitalii Novik <novickvitaliy@gmail.com>
2025-10-24 13:48:11 +0300

    implement api layer

--- src/services/BookCatalog/BookCatalog.Application/UseCases/Queries/Reviews/Get/GetReviewsQueryHandler.cs
+++ src/services/BookCatalog/BookCatalog.Application/UseCases/Queries/Reviews/Get/GetReviewsQueryHandler.cs
@@ -1,6 +1,7 @@
 using BookCatalog.Application.Contracts.Repositories;
 using BookCatalog.Application.DTOs.Reviews.Responses;
 using BookCatalog.Application.Mappers;
+using BookCatalog.Domain.Models;
 using Shared.CQRS.Queries;
 using Shared.DTOs;
 using Shared.ErrorHandling;
@@ -10,14 +11,21 @@ namespace BookCatalog.Application.UseCases.Queries.Reviews.Get;
 public sealed class GetReviewsQueryHandler : IQueryHandler<GetReviewsQuery, Result<PaginationResult<ReviewDto>>>
 {
     private readonly IReviewRepository _reviewRepository;
+    private readonly IBookRepository _bookRepository;
     
-    public GetReviewsQueryHandler(IReviewRepository reviewRepository)
+    public GetReviewsQueryHandler(IReviewRepository reviewRepository, IBookRepository bookRepository)
     {
         _reviewRepository = reviewRepository;
+        _bookRepository = bookRepository;
     }
     
     public async Task<Result<PaginationResult<ReviewDto>>> Handle(GetReviewsQuery request, CancellationToken cancellationToken)
     {
+        var book = await _bookRepository.GetByIdAsync(request.BookId, cancellationToken);
+        if (book is null)
+        {
+            return Result<PaginationResult<ReviewDto>>.NotFound(key: request.BookId, entityName: nameof(Book));
+        }
         var paginationResult = await _reviewRepository.GetAsync(request.BookId, request.Request.PageNumber, request.Request.PageSize, cancellationToken);
         
         return Result<PaginationResult<ReviewDto>>.Ok(paginationResult.ToPaginatedDtos(ReviewMapper.ToDto));
